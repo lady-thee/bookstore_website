b2b3a8f3b5cfd3932c6e01c38b304c0f9bfcc283
web-weebie <twinklet969@gmail.com>
2023-08-18 21:03:16 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+
+
+@dataclass
+class NewPerson:
+    name : str 
+    age : int 
+    city : str 
+
+
+p = NewPerson('Nobara', 23, 'japan') 
+print(p)
+print(p.name, p.age)
+
+print(hash(p))
+
