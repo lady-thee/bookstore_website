23c551da184b57371178e9f19c1965dbab002dc2
web-weebie <twinklet969@gmail.com>
2023-08-21 23:47:35 +0100

    Chore: Added pre-commits packages that modified config files

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 from dataclasses import dataclass
 
 
-
 @dataclass
 class NewPerson:
-    name : str 
-    age : int 
-    city : str 
+    name: str
+    age: int
+    city: str
 
 
-p = NewPerson('Nobara', 23, 'japan') 
+p = NewPerson("Nobara", 23, "japan")
 print(p)
 print(p.name, p.age)
 
 print(hash(p))
-
